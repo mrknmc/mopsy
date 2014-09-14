bee442ac167c3bfa090c1f4387fd9367561a3a31
Mark Nemec <mrknmc@me.com>
2014-09-14 11:15:07 +0100

    using Yeoman

--- gulp/tasks/bower.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var mainBowerFiles = require('main-bower-files');
-
-gulp.task('bower', function() {
-  return gulp.src(mainBowerFiles(), {
-      base: './bower_components'
-    })
-    .pipe(gulp.dest('build/vendor'));
-});
